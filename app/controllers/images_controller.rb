976dee4e0d80237909aab89c1423f2475b44a722
Apurva Bakshi <apurva.bakshi@appfolio.com>
2019-06-17 14:28:22 -0700

    Completed issue2

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,43 @@
+
+class ImagesController < ApplicationController
+  def new ;end
+
+  # @ is instance variable. Rails will pass instance var to view
+  def create
+    @image = Image.new(params.require(:image).permit(:url))
+
+    if check_url(@image.url)
+      flash[:success]="Image saved to the database successfully!"
+      @image.save
+      redirect_to @image
+    else
+      flash[:danger]="Invalid image url"
+      redirect_to(:action => 'new')
+    end
+  end
+
+  # Method which checks if urlvalue is a valid Http Image URL
+  def check_url(urlvalue)
+    url = URI.parse(urlvalue)
+    Net::HTTP.start(url.host, url.port) do |http|
+      response = http.head(url.path)
+      case response
+      when Net::HTTPSuccess, Net::HTTPRedirection
+        case response.content_type
+        when "image/png","image/gif","image/jpeg"
+          return true
+        else
+          return false
+        end
+      else
+        return false
+      end
+    end
+  rescue StandardError => e
+    return false
+  end
+
+  def show
+    @image = Image.find(params[:id])
+  end
+end
