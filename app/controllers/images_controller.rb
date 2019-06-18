db6ad9dfe0770dd23054e7217be4213e3c6b4a51
Apurva Bakshi <apurva.bakshi@appfolio.com>
2019-06-18 11:32:22 -0700

    Completed issue3

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,30 +1,31 @@
-
 class ImagesController < ApplicationController
-  def new ;end
+  def new
+    @image = Image.order('created_at DESC')
+  end
 
   # @ is instance variable. Rails will pass instance var to view
   def create
     @image = Image.new(params.require(:image).permit(:url))
 
     if check_url(@image.url)
-      flash[:success]="Image saved to the database successfully!"
+      flash[:success] = 'Image saved to the database successfully!'
       @image.save
       redirect_to @image
     else
-      flash[:danger]="Invalid image url"
-      redirect_to(:action => 'new')
+      flash[:danger] = 'Invalid image url'
+      redirect_to(action: 'new')
     end
   end
 
   # Method which checks if urlvalue is a valid Http Image URL
-  def check_url(urlvalue)
+  def check_url(urlvalue) # rubocop:disable Metrics/MethodLength
     url = URI.parse(urlvalue)
-    Net::HTTP.start(url.host, url.port) do |http|
+    Net::HTTP.start(url.host, url.port, use_ssl: true) do |http|
       response = http.head(url.path)
       case response
       when Net::HTTPSuccess, Net::HTTPRedirection
         case response.content_type
-        when "image/png","image/gif","image/jpeg"
+        when 'image/png', 'image/gif', 'image/jpeg'
           return true
         else
           return false
@@ -33,11 +34,13 @@ class ImagesController < ApplicationController
         return false
       end
     end
-  rescue StandardError => e
-    return false
+  rescue StandardError
+    false
   end
 
   def show
     @image = Image.find(params[:id])
   end
+
+  def index; end
 end
