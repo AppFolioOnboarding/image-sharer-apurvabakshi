92e3f7cc42256730d0b17503a0d75b97ce077709
Justin Pearson <justin.pearson@appfolio.com>
2019-07-09 12:26:04 -0700

    aab - Completed PostFeedbackService and its Test

--- app/javascript/image_sharing/services/PostFeedbackService.js
+++ app/javascript/image_sharing/services/PostFeedbackService.js
@@ -1,5 +1,19 @@
+import { post } from '../utils/helper';
+
 export class PostFeedbackService {
-  /* Implement your service */
+  constructor(store, parameters) {
+    this.store = store;
+    this.parameters = parameters;
+  }
+
+  postFeedback = () => post('/api/feedbacks', this.parameters)
+    .then(() => {
+    //  alert('Feedback submitted!');
+      this.store.clearData();
+    })
+    .catch(() => {
+    //  alert('Something went wrong, please try again later');
+    })
 }
 
 export default PostFeedbackService;
